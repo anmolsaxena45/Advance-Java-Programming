08c9214e0978cb3f5fdbef2b60e104a67aafb32f
anmolsaxena45 <anmolpoonam1976@gmail.com>
2026-04-12 22:16:52 +0530

    JDBC : File updated

--- 2026-04/05-04-26 : JDBC/EmployeeDB.java
+++ 2026-04/05-04-26 : JDBC/EmployeeDB.java
@@ -14,6 +14,9 @@ public class EmployeeDB {
 		String host = "jdbc:mysql://localhost:3306/employeeDB";
 		String username = "root";
 		String pwd = "anmol2004";
+		String query = "select * from employee where salary >= ";
+		long inSalary = 20000;
+		
 		String create_table = """
 				CREATE TABLE IF NOT EXISTS employee (
 				id INT PRIMARY KEY AUTO_INCREMENT,
@@ -29,48 +32,48 @@ public class EmployeeDB {
 			Connection con = DriverManager.getConnection(host, username, pwd);
 			System.out.println("Host is ready");
 			
-			Statement smt = con.createStatement(
-					ResultSet.TYPE_FORWARD_ONLY, //TYPE_SCROLL_INSENSITIVE - Scrolling forward is necessary to get previously scrolled data using .previous()
-					ResultSet.CONCUR_UPDATABLE); //CONCUR_READ_ONLY - Cannot modify Data
-			smt.execute(create_table);
-			System.out.println("Table is ready");
-			
-//			int rowAffected = smt.executeUpdate("""
-//					insert into employee (name, dept, salary) values ('Emp4', 'Sales', 20000), ('Emp5', 'Sales', 30000)
-//					""");
+//			Statement smt = con.createStatement(
+//					ResultSet.TYPE_FORWARD_ONLY, //TYPE_SCROLL_INSENSITIVE - Scrolling forward is necessary to get previously scrolled data using .previous()
+//					ResultSet.CONCUR_UPDATABLE); //CONCUR_READ_ONLY - Cannot modify Data
+//			smt.execute(create_table);
+//			System.out.println("Table is ready");
 //			
-//			if(rowAffected>0) System.out.println("Record inserted");
-//			else System.out.println("Record not inserted");
-			
-			ResultSet rs = smt.executeQuery("select * from employee");
-			System.out.println("ID\t Name\t Dept\t\t Salary");
-			
-			while(rs.next()) {
-				rs.moveToInsertRow();
-				rs.updateString("name", "Emp6");
-				rs.updateString("dept", "MCA");
-				rs.updateDouble("Salary", 80000);
-				rs.insertRow();
-				break;
-			}
-			
-			while(rs.next()) //rs.previous() will throw err here.
-			{
+////			int rowAffected = smt.executeUpdate("""
+////					insert into employee (name, dept, salary) values ('Emp4', 'Sales', 20000), ('Emp5', 'Sales', 30000)
+////					""");
+////			
+////			if(rowAffected>0) System.out.println("Record inserted");
+////			else System.out.println("Record not inserted");
+//			
+//			ResultSet rs = smt.executeQuery("select * from employee");
+//			System.out.println("ID\t Name\t Dept\t\t Salary");
+//			
+//			while(rs.next()) {
+//				rs.moveToInsertRow();
+//				rs.updateString("name", "Emp6");
+//				rs.updateString("dept", "MCA");
+//				rs.updateDouble("Salary", 80000);
+//				rs.insertRow();
+//				break;
+//			}
+//			
+//			while(rs.next()) //rs.previous() will throw err here.
+//			{
+////				long salary = rs.getLong("salary");
+////				if(salary<40000) {
+////					rs.updateDouble("salary", salary*1.1);
+////					rs.updateRow();
+////				}
+//				 int id = rs.getInt("id");
+//				String name = rs.getString("name");
+//				String dept = rs.getString("dept");
 //				long salary = rs.getLong("salary");
-//				if(salary<40000) {
-//					rs.updateDouble("salary", salary*1.1);
-//					rs.updateRow();
-//				}
-				 int id = rs.getInt("id");
-				String name = rs.getString("name");
-				String dept = rs.getString("dept");
-				long salary = rs.getLong("salary");
-//				if(salary<=40000) {
-//					rs.updateString("Salary", "Salary[Low]");
-//					
-				System.out.println(String.format("%d\t %s \t %s \t\t %d", id, name, dept, salary)); 
-//				}
-			}
+////				if(salary<=40000) {
+////					rs.updateString("Salary", "Salary[Low]");
+////					
+//				System.out.println(String.format("%d\t %s \t %s \t\t %d", id, name, dept, salary)); 
+////				}
+//			}
 			
 			
 			
@@ -85,3 +88,5 @@ public class EmployeeDB {
 //             ---------------------------------- ----------------------------------
 //            |                                  |                                  |
 //        Statement                      PreparedStatement                   CallableStatement
+//    These are completed             These are not completed
+//    statement.
